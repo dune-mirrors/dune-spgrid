f09b8cbec9f5fc10bd193452039d2eccca36e7f0
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-07-11 15:34:52 +0000

    created new project (by dune-project)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@2 f45f52a6-3457-4210-b8e0-e9d642ddcde3

---
+++ dune/grid/spgrid.hh
@@ -0,0 +1,6 @@
+#ifndef DUNE_SPGRID_HH
+#define DUNE_SPGRID_HH
+
+#include <dune/grid/spgrid/grid.hh>
+
+#endif // #ifndef DUNE_SPGRID_HH
---
+++ dune/grid/spgrid/grid.hh
@@ -0,0 +1,9 @@
+#ifndef DUNE_SPGRID_GRID_HH
+#define DUNE_SPGRID_GRID_HH
+
+namespace Dune
+{
+
+}
+
+#endif // #ifndef DUNE_SPGRID_GRID_HH
