81f225ef17f7db65aa9e2d6746d3618ccbc19f55
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-07-12 16:07:05 +0000

    added constructors for SPGridLevel
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@7 f45f52a6-3457-4210-b8e0-e9d642ddcde3

---
+++ dune/grid/spgrid/domain.hh
@@ -0,0 +1,39 @@
+#ifndef DUNE_SPGRID_DOMAIN_HH
+#define DUNE_SPGRID_DOMAIN_HH
+
+namespace Dune
+{
+
+  template< class ct, int dim >
+  class SPDomain
+  {
+    typedef SPDomain< ct, dim > This;
+
+  public:
+    typedef ct ctype;
+
+    static const int dimension = dim;
+
+    typedef FieldVector< ctype, dimension > GlobalVector;
+
+    SPDomain ( const GlobalVector &a, const GlobalVector &b )
+    {
+      for( int i = 0; i < dimension; ++i )
+      {
+        origin_[ i ] = std::min( a[ i ], b[ i ] );
+        width_[ i ] = std::max( a[ i ], b[ i ] ) - origin_[ i ];
+      }
+    }
+
+    const GlobalVector &width () const
+    {
+      return width_;
+    }
+
+  private:
+    GlobalVector origin_, width_;
+  };
+
+}
+
+#endif // #ifndef DUNE_SPGRID_DOMAIN_HH
--- dune/grid/spgrid/gridlevel.hh
+++ dune/grid/spgrid/gridlevel.hh
@@ -14,7 +14,43 @@ namespace Dune
   public:
     static const int dimension = dim;
 
+    typedef ct ctype;
+
+    typedef SPDomain< ct, dim > Domain;
+    typedef SPGridLevel< dt, dim > GridLevel;
+
+    typedef FieldVector< ctype, dimension > GlobalVector;
     typedef unsigned int MultiIndex[ dimension ];
+
+    SPGridLevel ( const Domain &domain, const MultiIndex &n )
+    : father_( 0 ),
+      domain_( &domain ),
+      level_( 0 )
+    {
+      const GlobalVector &width  = domain.width();
+      for( int i = 0; i < dimension; ++i )
+      {
+        n_[ i ] = n[ i ];
+        h_[ i ] = width[ i ] / (ctype)n[ i ];
+      }
+    }
+
+    SPGridLevel ( const GridLevel &father )
+    : father_( &father ),
+      domain_( father.domain_ ),
+      level_( father.level_+1 )
+    {
+      for( int i = 0; i < dimension; ++i )
+      {
+        n_[ i ] = 2*father.n_[ i ];
+        h_[ i ] = 0.5*father.h_[ i ];
+      }
+    }
+
+    const GlobalVector &h () const
+    {
+      return h_;
+    }
 
     unsigned int level () const
     {
@@ -28,8 +64,11 @@ namespace Dune
     }
 
   private:
+    const GridLevel *father_;
+    const Domain *domain_;
     unsigned int level_;
     MultiIndex n_;
+    GlobalVector h_;
     std::vector< MultiIndex > multiDirection_[ dimension+1 ];
   };
 
