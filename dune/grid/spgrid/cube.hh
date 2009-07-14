b8baa9d51756a51dfbfa78a18a88fe289cade7a9
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-07-14 08:57:15 +0000

    add our own cube
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@33 f45f52a6-3457-4210-b8e0-e9d642ddcde3

---
+++ dune/grid/spgrid/cube.hh
@@ -0,0 +1,80 @@
+#ifndef DUNE_SPGRID_CUBE_HH
+#define DUNE_SPGRID_CUBE_HH
+
+namespace Dune
+{
+
+  // SPCube
+  // ------
+
+  template< class ct, int dim >
+  class SPCube
+  {
+    typedef SPCube< ct, dim > This;
+
+  public:
+    typedef ct ctype;
+
+    static const int dimension = dim;
+
+    typedef FieldVector< ctype, dimension > GlobalVector;
+    typedef SPMultiIndex< dimension > MultiIndex;
+
+    static const int numCorners = (1 << dimension);
+
+    SPCube ()
+    {
+      for( int codim = 0; codim <= dimension; ++codim )
+      {
+        const unsigned int size = numSubEntities( dimension, codim );
+        subId_[ codim ].resize( size );
+        for( unsigned int i = 0; i < size; ++i )
+          subId( dimension, codim, i, subId_[ codim ][ i ] );
+      }
+
+      for( int i = 0; i < numCorners; ++i )
+      {
+        for( int j = 0; j < dimension; ++j )
+          corner_[ i ][ j ] = ctype( subId_[ dimension ][ i ][ j ] );
+      }
+    }
+
+  private:
+    unsigned int numSubEntities ( const unsigned int dim, const unsigned int codim )
+    {
+      assert( codim <= dim );
+      if( codim > 0 )
+      {
+        const unsigned int n0 = (codim < dim ? numSubEntities( dim-1, codim ) : 0);
+        const unsigned int n1 = numSubEntities( dim-1, codim-1 );
+        return n0 + 2*n1;
+      }
+      else
+        return 1;
+    }
+
+    void subId ( const unsigned int dim, const unsigned int codim,
+                 const unsigned int i, MultiIndex &subId ) const
+    {
+      assert( i < numSubEntities( dim, codim ) );
+      const unsigned int n0 = (codim < dim ? numSubEntities( dim-1, codim ) : 0);
+      if( i < n0 )
+      {
+        subId( dim-1, codim, i );
+        subId[ dim-1 ] = 1;
+      }
+      else
+      {
+        const unsigned int n1 = numSubEntities( dim-1, codim-1 );
+        subId( dim-1, codim-1, (i-n0)%n1 );
+        subId[ dim-1 ] = 2*((i-n0)/n1)
+      }
+    }
+
+    std::vector< MultiIndex > subId_[ dimension+1 ];
+    GlobalVector corner_[ numCorners ];
+  };
+
+}
+
+#endif
