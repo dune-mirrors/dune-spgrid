d826c98a5d121ac1a23dfb0a7de0e3fe5d2633f0
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-07-20 14:00:55 +0000

    added a (trivial) test
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@86 f45f52a6-3457-4210-b8e0-e9d642ddcde3

---
+++ test/gridcheck.cc
@@ -0,0 +1,31 @@
+#include <config.h>
+
+#ifndef DIMGRID
+#error "DIMGRID not defined. Please compile with -DDIMGRID=n"
+#endif
+
+#include <dune/grid/spgrid.hh>
+
+#include <dune/grid/test/gridcheck.cc>
+
+static const int dimGrid = DIMGRID;
+
+int main ( int argc, char **argv )
+{
+
+  typedef SPGrid< double, dimGrid > Grid;
+
+  FieldVector< double, dimGrid > a( 0.0 );
+  FieldVector< double, dimGrid > b( 1.0 );
+  int n[ dimGrid ];
+  for( int i = 0; i < dimGrid; ++i )
+    n[ i ] = 4;
+  Grid grid( a, b, n );
+
+  return 0;
+}
+catch( const Dune::Exception &e )
+{
+  std::cerr << e << std::endl;
+  return 1;
+}
