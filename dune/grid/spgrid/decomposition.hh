82a31d28b0d8ee07ad0779a01c68947ba2e7f909
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-11-20 15:48:57 +0000

    new structure: Partition = pair( origin, width )
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@164 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid/decomposition.hh
+++ dune/grid/spgrid/decomposition.hh
@@ -6,6 +6,40 @@
 namespace Dune
 {
 
+  // SPPartition
+  // -----------
+
+  template< int dim >
+  class SPPartition
+  {
+    typedef SPPartition< dim > This;
+
+  public:
+    static const int dimension = dim;
+
+    typedef SPMultiIndex< dimension > MultiIndex;
+
+    SPPartition ( const MultiIndex &origin, const MultiIndex &width )
+    : origin_( origin ),
+      width_( width )
+    {}
+
+    const MultiIndex &origin () const
+    {
+      return origin_;
+    }
+
+    const MultiIndex &width () const
+    {
+      return width_;
+    }
+
+  private:
+    MultiIndex origin_, width_;
+  };
+
+
+
   // SPDecomposition
   // ---------------
 
@@ -20,10 +54,11 @@ namespace Dune
     static const int dimension = dim;
 
     typedef SPMultiIndex< dimension > MultiIndex;
+    typedef SPPartition< dimension > Partition;
 
   public:
     SPDecomposition ( const unsigned int size, const MultiIndex &width )
-    : node_( new Node( size, width ) )
+    : node_( new Node( size, Partition( MultiIndex::zero(), width ) ) )
     {}
 
   private:
@@ -35,42 +70,40 @@ namespace Dune
   struct SPDecomposition< dim >::Node
   {
 
-    Node ( const unsigned int size, const MultiIndex &width,
-           const MultiIndex &origin = MultiIndex::zero() );
+    Node ( const Partition &partition, const unsigned int size )
 
     ~Node ();
 
   private:
+    Partition partition_;
     unsigned int size_;
-    MultiIndex width_, origin_;
     Node *left_, *right_;
   };
 
 
   template< int dim >
-  SPDecomposition< dim >::Node
-    ::Node ( const unsigned int size, const MultiIndex &width, const MultiIndex &origin )
-  : size_( size ),
-    width_( width ),
-    origin_( origin ),
+  SPDecomposition< dim >::Node::Node ( const Partition &partition, const unsigned int size )
+  : partition_( partition ),
+    size_( size ),
     left_( 0 ),
     right_( 0 )
   {
     if( size_ > 1 )
     {
-      const int dir = argmax( width_ );
+      const MultiIndex &width = partition_.width();
+      const int dir = argmax( width );
 
-      MultiIndex leftWidth = width_;
-      leftWidth[ dir ] = ((size/2) * width_[ dir ]) / size;
-      MultiIndex rightWidth = width_;
+      MultiIndex leftWidth = width;
+      leftWidth[ dir ] = ((size/2) * width[ dir ]) / size;
+      MultiIndex rightWidth = width;
       rightWidth[ dir ] -= leftWidth[ dir ];
 
-      MultiIndex leftOrigin = origin;
-      MultiIndex rightOrigin = origin;
+      MultiIndex leftOrigin = partition_.origin();
+      MultiIndex rightOrigin = partition_.origin();
       rightOrigin[ dir ] += leftWidth[ dir ];
 
-      left_ = new Node( size/2, leftWidth, leftOrigin );
-      right_ = new Node( size - size/2, rightWidth, rightOrigin );
+      left_ = new Node( size/2, Partition( leftOrigin, leftWidth ) );
+      right_ = new Node( size - size/2, Partition( rightOrigin, rightWidth ) );
     }
   }
 
