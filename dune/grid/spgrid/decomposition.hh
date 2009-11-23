3a6f6c44bed1b3284f04ebbbf8b97584eb629d3d
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-11-23 14:42:56 +0000

    make compile
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@179 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid/decomposition.hh
+++ dune/grid/spgrid/decomposition.hh
@@ -27,7 +27,7 @@ namespace Dune
     {}
 
     template< class ctype, SPRefinementStrategy strategy >
-    SPDomain ( const This &other, const SPRefinement< ctype, dimension, strategy > &refinement )
+    SPPartition ( const This &other, const SPRefinement< ctype, dimension, strategy > &refinement )
     : origin_( other.origin_ )
     {
       for( int i = 0; i < dimension; ++i )
@@ -66,13 +66,19 @@ namespace Dune
   {
     typedef SPDecomposition< dim > This;
 
-    struct Node;
+  public:
+    static const int dimension = dim;
+
+    typedef SPMultiIndex< dimension > MultiIndex;
+    typedef SPPartition< dimension > Partition;
+
+  private:
+    struct Node
     {
-      Node ( const Partition &partition, const unsigned int size )
+      Node ( const Partition &partition, const unsigned int size );
       ~Node ();
 
-      const Partition &Partition ( const unsigned int rank, const int overlap ) const;
-
+      const Partition &interiorPartition ( const unsigned int rank ) const;
       const Partition &allPartition () const;
 
     private:
@@ -82,49 +88,13 @@ namespace Dune
     };
 
   public:
-    static const int dimension = dim;
-
-    typedef SPMultiIndex< dimension > MultiIndex;
-    typedef SPPartition< dimension > Partition;
-
-  public:
     SPDecomposition ( const MultiIndex &width, const unsigned int size,
                       const unsigned int periodic = 0 )
     : root_( Partition( MultiIndex::zero(), width ), size ),
       periodic_( periodic )
     {}
 
-    Partition partition ( const unsigned int rank, const int overlap )
-    {
-      const Partition &interior = root_.interiorPartition( rank );
-      const Partition &allPartition = root_.allPartition();
-
-      const MultiIndex &allOrigin = allPartition.origin();
-      const MultiIndex &allWidth = allPartition.width();
-
-      MultiIndex origin = interior.origin();
-      MultiIndex width = interior.width();
-      for( int i = 0; i < dimension; ++i )
-      {
-        origin[ i ] -= overlap;
-        width[ i ] += 2*overlap;
-
-        if( !periodic( i ) )
-        {
-          const int lMargin = origin[ i ] - allOrigin[ i ];
-          if( lMartin < 0 )
-          {
-            origin[ i ] -= lMargin;
-            width[ i ] += lMargin;
-          }
-
-          const int rMargin = (allOrigin[ i ] + allWidth[ i ]) - (origin[ i ] + width[ i ]);
-          if( rMargin < 0 )
-            width[ i ] += rMargin;
-        }
-      }
-      return Partition( origin, width );
-    }
+    Partition partition ( const unsigned int rank, const int overlap ) const;
 
     bool periodic ( const int i ) const
     {
@@ -181,7 +151,7 @@ namespace Dune
     if( size_ > 1 )
     {
       assert( (left_ != 0) && (right_ != 0) );
-      if( rank_ < size_/2 )
+      if( rank < size_/2 )
         return left_->interiorPartition_( rank );
       else
         return right_->interiorPartition_( rank );
@@ -192,10 +162,45 @@ namespace Dune
 
 
   template< int dim >
-  const typename SPDecomposition< dim >::Partition &
+  inline const typename SPDecomposition< dim >::Partition &
   SPDecomposition< dim >::Node::allPartition () const
   {
     return partition_;
+  }
+
+
+  template< int dim >
+  inline typename SPDecomposition< dim >::Partition
+  SPDecomposition< dim >::partition ( const unsigned int rank, const int overlap ) const
+  {
+    const Partition &interior = root_.interiorPartition( rank );
+    const Partition &allPartition = root_.allPartition();
+
+    const MultiIndex &allOrigin = allPartition.origin();
+    const MultiIndex &allWidth = allPartition.width();
+
+    MultiIndex origin = interior.origin();
+    MultiIndex width = interior.width();
+    for( int i = 0; i < dimension; ++i )
+    {
+      origin[ i ] -= overlap;
+      width[ i ] += 2*overlap;
+
+      if( !periodic( i ) )
+      {
+        const int lMargin = origin[ i ] - allOrigin[ i ];
+        if( lMargin < 0 )
+        {
+          origin[ i ] -= lMargin;
+          width[ i ] += lMargin;
+        }
+
+        const int rMargin = (allOrigin[ i ] + allWidth[ i ]) - (origin[ i ] + width[ i ]);
+        if( rMargin < 0 )
+          width[ i ] += rMargin;
+      }
+    }
+    return Partition( origin, width );
   }
 
 }
