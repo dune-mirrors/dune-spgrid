02c6cb703dfcbab22b2b7f0fe0c96cb5851291d1
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-11-24 13:15:45 +0000

    use allPartition of determine iterator bounds
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@198 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid/iterator.hh
+++ dune/grid/spgrid/iterator.hh
@@ -21,6 +21,8 @@ namespace Dune
     typedef typename Base::EntityInfo EntityInfo;
     typedef typename Base::GridLevel GridLevel;
 
+    typedef typename GridLevel::Partition Partition;
+
     static const int dimension = Base::dimension;
     static const int codimension = Base::codimension;
     static const int mydimension = Base::mydimension;
@@ -36,91 +38,19 @@ namespace Dune
   public:
     using Base::gridLevel;
 
-    SPIterator ( const GridLevel &gridLevel, const Begin &b,
-                 const unsigned int sweepDir = 0 )
-    : Base( gridLevel ),
-      sweepDirection_( sweepDir )
-    {
-      assert( sweepDir < numDirections );
-      EntityInfo &entityInfo = Grid::getRealImplementation( entity_ ).entityInfo();
+    SPIterator ( const GridLevel &gridLevel, const Begin &b, const unsigned int sweepDir = 0 );
+    SPIterator ( const GridLevel &gridLevel, const End &e, const unsigned int sweepDir = 0 );
 
-      unsigned int dir = 0;
-      const unsigned int mydim = mydimension;
-      for( ; (dir < numDirections) && (bitCount( dir ) != mydim); ++dir );
-      assert( dir < numDirections );
-
-      MultiIndex &id = entityInfo.id();
-      for( int i = 0; i < dimension; ++i )
-        id[ i ] = begin( i, dir );
-      entityInfo.update();
-      assert( entityInfo.direction() == dir );
-    }
-
-    SPIterator ( const GridLevel &gridLevel, const End &e,
-                 const unsigned int sweepDir = 0 )
-    : Base( gridLevel ),
-      sweepDirection_( sweepDir )
-    {
-      assert( sweepDir < numDirections );
-      EntityInfo &entityInfo = Grid::getRealImplementation( entity_ ).entityInfo();
-
-      unsigned int dir = numDirections-1;
-      const unsigned int mydim = mydimension;
-      for( ; (dir < numDirections) && (bitCount( dir ) != mydim); --dir );
-      assert( dir < numDirections );
-
-      MultiIndex &id = entityInfo.id();
-      for( int i = 0; i < dimension-1; ++i )
-        id[ i ] = begin( i, dir );
-      id[ dimension-1 ] = end( dimension-1, dir );
-      entityInfo.update();
-    }
-
-    void increment ()
-    {
-      EntityInfo &entityInfo = Grid::getRealImplementation( entity_ ).entityInfo();
-
-      MultiIndex &id = entityInfo.id();
-      assert( id[ dimension-1 ] != end( dimension-1, entityInfo.direction() ) );
-      for( int i = 0; i < dimension; ++i )
-      {
-        const unsigned int sweep = (sweepDirection_ >> i) & 1;
-        id[ i ] += (2 - 4*sweep);
-        if( id[ i ] != end( i, entityInfo.direction() ) )
-          return entityInfo.update();
-        id[ i ] = begin( i, entityInfo.direction() );
-      }
-
-      unsigned int dir = entityInfo.direction()+1;
-      const unsigned int mydim = mydimension;
-      for( ; (dir < numDirections) && (bitCount( dir ) != mydim); ++dir );
-      if( dir < numDirections )
-      {
-        for( int i = 0; i < dimension; ++i )
-          id[ i ] = begin( i, dir );
-      }
-      else
-        id[ dimension-1 ] = end( dimension-1, entityInfo.direction() );
-
-      entityInfo.update();
-    }
+    void increment ();
 
   private:
-    const int begin ( const int i, const unsigned int dir ) const
+    const Partition &partition () const
     {
-      const MultiIndex &cells = gridLevel().cells();
-      const unsigned int sweep = (sweepDirection_ >> i) & 1;
-      const unsigned int d = (dir >> i) & 1;
-      return d + sweep*2*(cells[ i ]-d);
+      return gridLevel().allPartition();
     }
 
-    const int end ( const int i, const unsigned int dir ) const
-    {
-      const MultiIndex &cells = gridLevel().cells();
-      const unsigned int sweep = (sweepDirection_ >> i) & 1;
-      const unsigned int d = (dir >> i) & 1;
-      return (d-2) + (1-sweep)*2*(cells[ i ]-(d-2));
-    }
+    int begin ( const int i, const unsigned int dir ) const;
+    int end ( const int i, const unsigned int dir ) const;
 
   protected:
     using Base::entity_;
@@ -128,6 +58,113 @@ namespace Dune
   private:
     unsigned int sweepDirection_;
   };
+
+
+
+  // Implementation of SPIterator
+  // ----------------------------
+
+  template< int codim, PartitionIteratorType pitype, class Grid >
+  inline SPIterator< codim, pitype, Grid >
+    ::SPIterator ( const GridLevel &gridLevel, const Begin &b, const unsigned int sweepDir )
+  : Base( gridLevel ),
+    sweepDirection_( sweepDir )
+  {
+    assert( sweepDir < numDirections );
+    EntityInfo &entityInfo = Grid::getRealImplementation( entity_ ).entityInfo();
+
+    unsigned int dir = 0;
+    const unsigned int mydim = mydimension;
+    for( ; (dir < numDirections) && (bitCount( dir ) != mydim); ++dir );
+    assert( dir < numDirections );
+
+    MultiIndex &id = entityInfo.id();
+    for( int i = 0; i < dimension; ++i )
+      id[ i ] = begin( i, dir );
+    entityInfo.update();
+    assert( entityInfo.direction() == dir );
+  }
+
+
+  template< int codim, PartitionIteratorType pitype, class Grid >
+  inline SPIterator< codim, pitype, Grid >
+    ::SPIterator ( const GridLevel &gridLevel, const End &e, const unsigned int sweepDir )
+  : Base( gridLevel ),
+    sweepDirection_( sweepDir )
+  {
+    assert( sweepDir < numDirections );
+    EntityInfo &entityInfo = Grid::getRealImplementation( entity_ ).entityInfo();
+
+    unsigned int dir = numDirections-1;
+    const unsigned int mydim = mydimension;
+    for( ; (dir < numDirections) && (bitCount( dir ) != mydim); --dir );
+    assert( dir < numDirections );
+
+    MultiIndex &id = entityInfo.id();
+    for( int i = 0; i < dimension-1; ++i )
+      id[ i ] = begin( i, dir );
+    id[ dimension-1 ] = end( dimension-1, dir );
+    entityInfo.update();
+  }
+
+
+  template< int codim, PartitionIteratorType pitype, class Grid >
+  inline void SPIterator< codim, pitype, Grid >::increment ()
+  {
+    EntityInfo &entityInfo = Grid::getRealImplementation( entity_ ).entityInfo();
+
+    MultiIndex &id = entityInfo.id();
+    assert( id[ dimension-1 ] != end( dimension-1, entityInfo.direction() ) );
+    for( int i = 0; i < dimension; ++i )
+    {
+      const unsigned int sweep = (sweepDirection_ >> i) & 1;
+      id[ i ] += (2 - 4*sweep);
+      if( id[ i ] != end( i, entityInfo.direction() ) )
+        return entityInfo.update();
+      id[ i ] = begin( i, entityInfo.direction() );
+    }
+
+    unsigned int dir = entityInfo.direction()+1;
+    const unsigned int mydim = mydimension;
+    for( ; (dir < numDirections) && (bitCount( dir ) != mydim); ++dir );
+    if( dir < numDirections )
+    {
+      for( int i = 0; i < dimension; ++i )
+        id[ i ] = begin( i, dir );
+    }
+    else
+      id[ dimension-1 ] = end( dimension-1, entityInfo.direction() );
+
+    entityInfo.update();
+  }
+
+
+  template< int codim, PartitionIteratorType pitype, class Grid >
+  inline int
+  SPIterator< codim, pitype, Grid >::begin ( const int i, const unsigned int dir ) const
+  {
+    //const MultiIndex &cells = gridLevel().cells();
+    const MultiIndex &begin = partition().begin();
+    const MultiIndex &end = partition().end();
+    const unsigned int sweep = (sweepDirection_ >> i) & 1;
+    const unsigned int d = (dir >> i) & 1;
+    //return d + sweep*2*(cells[ i ]-d);
+    return (1-sweep)*(2*begin[ i ] + d) + sweep*(2*end[ i ] - d);
+  }
+
+
+  template< int codim, PartitionIteratorType pitype, class Grid >
+  inline int
+  SPIterator< codim, pitype, Grid >::end ( const int i, const unsigned int dir ) const
+  {
+    //const MultiIndex &cells = gridLevel().cells();
+    const MultiIndex &begin = partition().begin();
+    const MultiIndex &end = partition().end();
+    const unsigned int sweep = (sweepDirection_ >> i) & 1;
+    const unsigned int d = (dir >> i) & 1;
+    //return (d-2) + (1-sweep)*2*(cells[ i ]-(d-2));
+    return (1-sweep)*(2*end[ i ] - (d-2)) + sweep*(2*begin[ i ] + (d-2));
+  }
 
 }
 
