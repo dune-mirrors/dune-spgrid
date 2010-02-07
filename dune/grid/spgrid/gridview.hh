52f66cf5043eebe279f9681c6872b4a12fd34089
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-02-07 14:05:40 +0000

    move implementation out of class
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@345 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid/gridview.hh
+++ dune/grid/spgrid/gridview.hh
@@ -144,163 +144,283 @@ namespace Dune
 
     typedef SPIntersectionIterator< const Grid > IntersectionIteratorImpl;
 
-    SPGridView ()
-    : indexSet_( new IndexSetPair )
-    {
-      indexSet_->second = 1;
-    }
+    SPGridView ();
 
-    explicit SPGridView ( const GridLevel &gridLevel )
-    : indexSet_( new IndexSetPair )
-    {
-      indexSet_->first.update( gridLevel );
-      indexSet_->second = 1;
-    }
+    explicit SPGridView ( const GridLevel &gridLevel );
 
   public:
     template< class VT >
-    SPGridView ( const SPGridView< VT > &other )
-    : indexSet_( other.indexSet_ )
-    {
-      ++indexSet_->second;
-    }
+    SPGridView ( const SPGridView< VT > &other );
 
-    SPGridView ( const This &other )
-    : indexSet_( other.indexSet_ )
-    {
-      ++indexSet_->second;
-    }
+    SPGridView ( const This &other );
 
-    ~SPGridView ()
-    {
-      if( --indexSet_->second == 0 )
-        delete indexSet_;
-    }
+    ~SPGridView ();
 
-    This &operator= ( const This &other )
-    {
-      ++other.indexSet_->second;
-      if( --indexSet_->second == 0 )
-        delete indexSet_;
-      indexSet_ = other.indexSet_;
-      return *this;
-    }
+    This &operator= ( const This &other );
 
-    const Grid &grid () const
-    {
-      return gridLevel().grid();
-    }
+    const Grid &grid () const;
 
-    const IndexSet &indexSet () const
-    {
-      return indexSet_->first;
-    }
+    const IndexSet &indexSet () const;
 
-    int size ( int codim ) const
-    {
-      return indexSet().size( codim );
-    }
+    int size ( int codim ) const;
+    int size ( const GeometryType &type ) const;
 
-    int size ( const GeometryType &type ) const
-    {
-      return indexSet().size( type );
-    }
+    int overlapSize ( const int codim ) const;
+    int ghostSize ( const int codim ) const;
 
     template< int codim >
     typename Codim< codim >::Iterator
-    begin ( const unsigned int sweepDir = 0 ) const
-    {
-      typedef typename Codim< codim >::IteratorImpl IteratorImpl;
-      typename IteratorImpl::Begin begin;
-      return IteratorImpl( gridLevel(), begin, sweepDir );
-    }
+    begin ( const unsigned int sweepDir = 0 ) const;
 
     template< int codim >
     typename Codim< codim >::Iterator
-    end ( const unsigned int sweepDir = 0 ) const
-    {
-      typedef typename Codim< codim >::IteratorImpl IteratorImpl;
-      typename IteratorImpl::End end;
-      return IteratorImpl( gridLevel(), end, sweepDir );
-    }
+    end ( const unsigned int sweepDir = 0 ) const;
 
     template< int codim, PartitionIteratorType pitype >
     typename Codim< codim >::template Partition< pitype >::Iterator
-    begin ( const unsigned int sweepDir = 0 ) const
-    {
-      typedef typename Codim< codim >::template Partition< pitype >::IteratorImpl IteratorImpl;
-      typename IteratorImpl::Begin begin;
-      return IteratorImpl( gridLevel(), begin, sweepDir );
-    }
+    begin ( const unsigned int sweepDir = 0 ) const;
 
     template< int codim, PartitionIteratorType pitype >
     typename Codim< codim >::template Partition< pitype >::Iterator
-    end ( const unsigned int sweepDir = 0 ) const
-    {
-      typedef typename Codim< codim >::template Partition< pitype >::IteratorImpl IteratorImpl;
-      typename IteratorImpl::End end;
-      return IteratorImpl( gridLevel(), end, sweepDir );
-    }
+    end ( const unsigned int sweepDir = 0 ) const;
 
-    IntersectionIterator ibegin ( const typename Codim< 0 >::Entity &entity ) const
-    {
-      return IntersectionIteratorImpl( entity, 0 );
-    }
-
-    IntersectionIterator iend ( const typename Codim< 0 >::Entity &entity ) const
-    {
-      return IntersectionIteratorImpl( entity, GridLevel::Cube::numFaces );
-    }
+    IntersectionIterator ibegin ( const typename Codim< 0 >::Entity &entity ) const;
+    IntersectionIterator iend ( const typename Codim< 0 >::Entity &entity ) const;
 
     template< int codim >
     typename Codim< codim >::SuperEntityIterator
-    superEntityBegin ( const typename Codim< codim >::Entity &entity ) const
-    {
-      typedef SPSuperEntityIterator< const Grid > Impl;
-      return Impl( Grid::getRealImplementation( entity ), typename Impl::Begin() );
-    }
+    superEntityBegin ( const typename Codim< codim >::Entity &entity ) const;
 
     template< int codim >
     typename Codim< codim >::SuperEntityIterator
-    superEntityEnd ( const typename Codim< codim >::Entity &entity ) const
-    {
-      typedef SPSuperEntityIterator< const Grid > Impl;
-      return Impl( Grid::getRealImplementation( entity ), typename Impl::End() );
-    }
+    superEntityEnd ( const typename Codim< codim >::Entity &entity ) const;
 
-    const CollectiveCommunication &comm () const
-    {
-      return grid().comm();
-    }
-
-    int overlapSize ( const int codim ) const
-    {
-      return 0;
-    }
-
-    int ghostSize ( const int codim ) const
-    {
-      return 0;
-    }
+    const CollectiveCommunication &comm () const;
 
     template< class DataHandle, class Data >
     void communicate ( CommDataHandleIF< DataHandle, Data > &data,
-                       InterfaceType interface, CommunicationDirection dir ) const
-    {}
+                       InterfaceType interface, CommunicationDirection dir ) const;
 
-    const GridLevel &gridLevel () const
-    {
-      return indexSet().gridLevel();
-    }
+    const GridLevel &gridLevel () const;
 
-    void update ( const GridLevel &gridLevel )
-    {
-      indexSet_->first.update( gridLevel );
-    }
+    void update ( const GridLevel &gridLevel );
 
   private:
     IndexSetPair *indexSet_;
   };
+
+
+
+  // Implementation of SPGridView
+  // ----------------------------
+
+  template< class ViewTraits >
+  inline SPGridView< ViewTraits >::SPGridView ()
+  : indexSet_( new IndexSetPair )
+  {
+    indexSet_->second = 1;
+  }
+
+
+  template< class ViewTraits >
+  inline SPGridView< ViewTraits >::SPGridView ( const GridLevel &gridLevel )
+  : indexSet_( new IndexSetPair )
+  {
+    indexSet_->first.update( gridLevel );
+    indexSet_->second = 1;
+  }
+
+
+  template< class ViewTraits >
+  template< class VT >
+  inline SPGridView< ViewTraits >::SPGridView ( const SPGridView< VT > &other )
+  : indexSet_( other.indexSet_ )
+  {
+    ++indexSet_->second;
+  }
+
+
+  template< class ViewTraits >
+  inline SPGridView< ViewTraits >::SPGridView ( const This &other )
+  : indexSet_( other.indexSet_ )
+  {
+    ++indexSet_->second;
+  }
+
+
+  template< class ViewTraits >
+  inline SPGridView< ViewTraits >::~SPGridView ()
+  {
+    if( --indexSet_->second == 0 )
+      delete indexSet_;
+  }
+
+
+  template< class ViewTraits >
+  inline typename SPGridView< ViewTraits >::This &
+  SPGridView< ViewTraits >::operator= ( const This &other )
+  {
+    ++other.indexSet_->second;
+    if( --indexSet_->second == 0 )
+      delete indexSet_;
+    indexSet_ = other.indexSet_;
+    return *this;
+  }
+
+
+  template< class ViewTraits >
+  inline const typename SPGridView< ViewTraits >::Grid &
+  SPGridView< ViewTraits >::grid () const
+  {
+    return gridLevel().grid();
+  }
+
+
+  template< class ViewTraits >
+  inline const typename SPGridView< ViewTraits >::IndexSet &
+  SPGridView< ViewTraits >::indexSet () const
+  {
+    return indexSet_->first;
+  }
+
+
+  template< class ViewTraits >
+  inline int SPGridView< ViewTraits >::size ( int codim ) const
+  {
+    return indexSet().size( codim );
+  }
+
+
+  template< class ViewTraits >
+  inline int SPGridView< ViewTraits >::size ( const GeometryType &type ) const
+  {
+    return indexSet().size( type );
+  }
+
+
+  template< class ViewTraits >
+  inline int SPGridView< ViewTraits >::overlapSize ( const int codim ) const
+  {
+    return 0;
+  }
+
+
+  template< class ViewTraits >
+  inline int SPGridView< ViewTraits >::ghostSize ( const int codim ) const
+  {
+    return 0;
+  }
+
+
+  template< class ViewTraits >
+  template< int codim >
+  inline typename SPGridView< ViewTraits >::template Codim< codim >::Iterator
+  SPGridView< ViewTraits >::begin ( const unsigned int sweepDir ) const
+  {
+    typedef typename Codim< codim >::IteratorImpl IteratorImpl;
+    typename IteratorImpl::Begin begin;
+    return IteratorImpl( gridLevel(), begin, sweepDir );
+  }
+
+
+  template< class ViewTraits >
+  template< int codim >
+  inline typename SPGridView< ViewTraits >::template Codim< codim >::Iterator
+  SPGridView< ViewTraits >::end ( const unsigned int sweepDir ) const
+  {
+    typedef typename Codim< codim >::IteratorImpl IteratorImpl;
+    typename IteratorImpl::End end;
+    return IteratorImpl( gridLevel(), end, sweepDir );
+  }
+
+
+  template< class ViewTraits >
+  template< int codim, PartitionIteratorType pitype >
+  inline typename SPGridView< ViewTraits >::template Codim< codim >::template Partition< pitype >::Iterator
+  SPGridView< ViewTraits >::begin ( const unsigned int sweepDir ) const
+  {
+    typedef typename Codim< codim >::template Partition< pitype >::IteratorImpl IteratorImpl;
+    typename IteratorImpl::Begin begin;
+    return IteratorImpl( gridLevel(), begin, sweepDir );
+  }
+
+
+  template< class ViewTraits >
+  template< int codim, PartitionIteratorType pitype >
+  inline typename SPGridView< ViewTraits >::template Codim< codim >::template Partition< pitype >::Iterator
+  SPGridView< ViewTraits >::end ( const unsigned int sweepDir ) const
+  {
+    typedef typename Codim< codim >::template Partition< pitype >::IteratorImpl IteratorImpl;
+    typename IteratorImpl::End end;
+    return IteratorImpl( gridLevel(), end, sweepDir );
+  }
+
+
+  template< class ViewTraits >
+  inline typename SPGridView< ViewTraits >::IntersectionIterator
+  SPGridView< ViewTraits >::ibegin ( const typename Codim< 0 >::Entity &entity ) const
+  {
+    return IntersectionIteratorImpl( entity, 0 );
+  }
+
+
+  template< class ViewTraits >
+  inline typename SPGridView< ViewTraits >::IntersectionIterator
+  SPGridView< ViewTraits >::iend ( const typename Codim< 0 >::Entity &entity ) const
+  {
+    return IntersectionIteratorImpl( entity, GridLevel::Cube::numFaces );
+  }
+
+
+  template< class ViewTraits >
+  template< int codim >
+  inline typename SPGridView< ViewTraits >::template Codim< codim >::SuperEntityIterator
+  SPGridView< ViewTraits >::superEntityBegin ( const typename Codim< codim >::Entity &entity ) const
+  {
+    typedef SPSuperEntityIterator< const Grid > Impl;
+    return Impl( Grid::getRealImplementation( entity ), typename Impl::Begin() );
+  }
+
+
+  template< class ViewTraits >
+  template< int codim >
+  inline typename SPGridView< ViewTraits >::template Codim< codim >::SuperEntityIterator
+  SPGridView< ViewTraits >::superEntityEnd ( const typename Codim< codim >::Entity &entity ) const
+  {
+    typedef SPSuperEntityIterator< const Grid > Impl;
+    return Impl( Grid::getRealImplementation( entity ), typename Impl::End() );
+  }
+
+
+  template< class ViewTraits >
+  inline const typename SPGridView< ViewTraits >::CollectiveCommunication &
+  SPGridView< ViewTraits >::comm () const
+  {
+    return grid().comm();
+  }
+
+
+  template< class ViewTraits >
+  template< class DataHandle, class Data >
+  inline void SPGridView< ViewTraits >
+    ::communicate ( CommDataHandleIF< DataHandle, Data > &data,
+                    InterfaceType interface, CommunicationDirection dir ) const
+  {}
+
+
+  template< class ViewTraits >
+  inline const typename SPGridView< ViewTraits >::GridLevel &
+  SPGridView< ViewTraits >::gridLevel () const
+  {
+    return indexSet().gridLevel();
+  }
+
+
+  template< class ViewTraits >
+  inline void SPGridView< ViewTraits >::update ( const GridLevel &gridLevel )
+  {
+    indexSet_->first.update( gridLevel );
+  }
 
 }
 
