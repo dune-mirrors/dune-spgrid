f2ff563e820599297cdd140725ebdbb928dc23ff
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-02-08 12:42:14 +0000

    add gdb support to script
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@360 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- test/gridcheck.sh
+++ test/gridcheck.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 vpid="$OMPI_MCA_ns_nds_vpid"
-./gridcheck "$@" &> gridcheck.$vpid.out
+if test "$1" = "gdb" ; then
+  shift 1
+  xterm -e gdb --eval-command=run --args ./gridcheck "$@"
+else
+  ./gridcheck "$@" &> gridcheck.$vpid.out
+fi
