e13af62693cde419a3af39bac0725ef2e06275f3
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-02-11 07:44:09 +0000

    improve parallel scripts (move output into separate subdirectory)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@371 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- test/seitcheck.sh
+++ test/seitcheck.sh
@@ -6,8 +6,8 @@ vpid="$OMPI_MCA_ns_nds_vpid"
 size="$OMPI_MCA_ns_nds_num_procs"
 if test "$1" = "gdb" ; then
   shift 1
-  xterm -e gdb --eval-command=run --args ./gridcheck "$@"
+  xterm -e gdb --eval-command=run --args ./seitcheck "$@"
 else
   mkdir -p $base/mpiout
-  ./gridcheck "$@" &> $base/mpiout/gridcheck-$size-$vpid
+  ./seitcheck "$@" &> $base/mpiout/seitcheck-$size-$vpid
 fi
