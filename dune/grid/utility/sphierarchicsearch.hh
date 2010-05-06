fe7f6ab528f748bbb249fe93f82f3d959fac4319
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-05-06 07:53:06 +0000

    actually specialize by derivation
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@407 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/utility/sphierarchicsearch.hh
+++ dune/grid/utility/sphierarchicsearch.hh
@@ -20,22 +20,23 @@ namespace Dune
 
 
 
-  // HierarchicSearch for SPGrid
-  // ---------------------------
+  // SPHierarchicSearch
+  // ------------------
 
-  template< class ct, int dim, SPRefinementStrategy strategy, class Comm, class IndexSet >
-  class HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >
+  template< class Grid, class IndexSet >
+  struct SPHierarchicSearch
   {
-    typedef SPGrid< ct, dim, strategy, Comm > Grid;
+    typedef typename Grid::ctype ctype;
 
-  public:
-    typedef FieldVector< ct, dim > GlobalVector;
-    typedef FieldVector< ct, dim > LocalVector;
+    static const int dimension = Grid::dimension;
+
+    typedef FieldVector< ctype, dimension > GlobalVector;
+    typedef GlobalVector LocalVector;
     
     typedef typename Grid::template Codim< 0 >::Entity Entity;
     typedef typename Grid::template Codim< 0 >::EntityPointer EntityPointer;
 
-    HierarchicSearch ( const Grid &grid, const IndexSet &indexSet );
+    SPHierarchicSearch ( const Grid &grid, const IndexSet &indexSet );
 
     EntityPointer findEntity ( const GlobalVector &global ) const;
     
@@ -48,18 +49,37 @@ namespace Dune
 
 
 
+
+  // HierarchicSearch for SPGrid
+  // ---------------------------
+
   template< class ct, int dim, SPRefinementStrategy strategy, class Comm, class IndexSet >
-  inline HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >
-    ::HierarchicSearch ( const Grid &grid, const IndexSet &indexSet )
+  class HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >
+  : public SPHierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >
+  {
+    typedef SPHierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet > Base;
+    typedef SPGrid< ct, dim, strategy, Comm > Grid;
+
+  public:
+    HierarchicSearch ( const Grid &grid, const IndexSet &indexSet );
+  };
+
+
+
+  // Implementation of SPHierarchicSearch
+  // ------------------------------------
+
+  template< class Grid, class IndexSet >
+  inline SPHierarchicSearch< Grid, IndexSet >
+    ::SPHierarchicSearch ( const Grid &grid, const IndexSet &indexSet )
   : grid_( grid ),
     indexSet_( indexSet )
   {}
 
 
-  template< class ct, int dim, SPRefinementStrategy strategy, class Comm, class IndexSet >
-  inline typename HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >::EntityPointer
-  HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >
-    ::findEntity ( const GlobalVector &global ) const
+  template< class Grid, class IndexSet >
+  inline typename SPHierarchicSearch< Grid, IndexSet >::EntityPointer
+  SPHierarchicSearch< Grid, IndexSet >::findEntity ( const GlobalVector &global ) const
   {
     EntityPointer ep = grid_.findEntity( global, 0 );
     if( indexSet_.contains( *ep ) )
@@ -70,18 +90,19 @@ namespace Dune
 
 
 
-  template< class ct, int dim, SPRefinementStrategy strategy, class Comm, class IndexSet >
-  inline typename HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >::EntityPointer
-  HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >
+  template< class Grid, class IndexSet >
+  inline typename SPHierarchicSearch< Grid, IndexSet >::EntityPointer
+  SPHierarchicSearch< Grid, IndexSet >
     ::hFindEntity ( const Entity &e, const GlobalVector &global ) const
   {
     typedef typename Grid::template Codim< 0 >::HierarchicIterator HierarchicIterator;
+
     const HierarchicIterator end = e.hend( e.level()+1 );
     for( HierarchicIterator it = e.hbegin( e.level()+1 ); it != end; ++it )
     {
       const Entity &child = *it;
       LocalVector local = child.geometry().local( global );
-      if( GenericReferenceElements< double, dim >::cube().checkInside( local ) )
+      if( GenericReferenceElements< ctype, dimension >::cube().checkInside( local ) )
       {
         if( indexSet_.contains( *it ) )
           return EntityPointer( it );
@@ -91,6 +112,17 @@ namespace Dune
     }
     DUNE_THROW( Exception, "Unexpected internal Error" );
   }
+
+
+
+  // Implementation of HierarchicSearch for SPGrid
+  // ---------------------------------------------
+
+  template< class ct, int dim, SPRefinementStrategy strategy, class Comm, class IndexSet >
+  inline HierarchicSearch< SPGrid< ct, dim, strategy, Comm >, IndexSet >
+    ::HierarchicSearch ( const Grid &grid, const IndexSet &indexSet )
+  : Base( grid, indexSet )
+  {}
 
 }
 
