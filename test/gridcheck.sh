f49f8446cca643f5f536d9d88f855c5a3566e82f
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-07-16 12:08:47 +0000

    adapt to newer OpenMPI
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@433 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- test/gridcheck.sh
+++ test/gridcheck.sh
@@ -2,8 +2,20 @@
 
 base=`dirname $0`
 
-vpid="$OMPI_MCA_ns_nds_vpid"
-size="$OMPI_MCA_ns_nds_num_procs"
+vpid="$OMPI_COMM_WORLD_RANK"
+size="$OMPI_COMM_WORLD_SIZE"
+
+if test -z "$size" ; then
+  echo "Unable to determine number of processors."
+  set
+  exit 1;
+fi
+
+if test -z "$vpid" ; then
+  echo "Unable to determine process rank."
+  exit 1;
+fi
+
 if test "$1" = "gdb" ; then
   shift 1
   xterm -e gdb --eval-command=run --args ./gridcheck "$@"
