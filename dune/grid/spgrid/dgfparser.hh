14a1837182e019b3c302f3dac14ce2d59dba2903
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-11-01 15:10:15 +0000

    directly call implementation of boundaryId (to avoid deprecation warning)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@469 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid/dgfparser.hh
+++ dune/grid/spgrid/dgfparser.hh
@@ -94,7 +94,7 @@ namespace Dune
     template< class Intersection >
     int boundaryId ( const Intersection &intersection ) const
     {
-      return intersection.boundaryId();
+      return Grid::getRealImplementation( intersection ).boundaryId();
     }
 
     template< int codim >
