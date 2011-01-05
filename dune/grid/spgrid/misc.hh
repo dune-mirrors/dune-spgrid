b389ebe971c92e3f47546993268177c7e6b530f0
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2011-01-05 09:20:47 +0000

    document misc.hh
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@492 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid/misc.hh
+++ dune/grid/spgrid/misc.hh
@@ -1,9 +1,20 @@
 #ifndef DUNE_SPGRID_MISC_HH
 #define DUNE_SPGRID_MISC_HH
 
+/** \file
+ *  \author Martin Nolte
+ *  \brief  miscellaneous helper functions
+ */
+
 namespace Dune
 {
 
+  /** \brief count the number of set bits within an integer
+   *
+   *  \param[in]  i  integer whose set bits shall be counted
+   *
+   *  \returns number of set bits in the integer i
+   */
   inline unsigned int bitCount ( unsigned int i )
   {
     unsigned int bitCount = 0;
@@ -14,12 +25,32 @@ namespace Dune
 
 
 
+  /** \brief obtain index with greater value in array
+   *
+   *  \param[in]  array  array of comparable values
+   *  \param[in]  i      first index
+   *  \param[in]  j      second index
+   *
+   *  \note The array values must implement the operator <.
+   *
+   *  \returns i if array[ i ] > array[ j ], j otherwise
+   */
   template< class Array, class Index >
   inline Index argmax( const Array &array, Index i, Index j )
   {
-    return (array[ i ] > array[ j ] ? i : j);
+    return (array[ j ] < array[ i ] ? i : j);
   }
 
+  /** \brief obtain index with lesser value in array
+   *
+   *  \param[in]  array  array of comparable values
+   *  \param[in]  i      first index
+   *  \param[in]  j      second index
+   *
+   *  \note The array values must implement the operator <.
+   *
+   *  \returns i if array[ i ] < array[ j ], j otherwise
+   */
   template< class Array, class Index >
   inline Index argmin( const Array &array, Index i, Index j )
   {
