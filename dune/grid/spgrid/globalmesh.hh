9d6356b5ed059e177380059834ad7a0eae32595f
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2011-02-22 08:27:08 +0000

    document globalmesh.hh
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@514 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid/globalmesh.hh
+++ dune/grid/spgrid/globalmesh.hh
@@ -8,13 +8,24 @@
 #include <dune/grid/spgrid/multiindex.hh>
 #include <dune/grid/spgrid/refinement.hh>
 
+/** \file
+ *  \author Martin Nolte
+ *  \brief  description of a mesh for the infinite space
+ */
+
 namespace Dune
 {
 
+  /** \class SPGlobalMesh
+   *  \brief description of a mesh for the infinite space
+   *
+   *  \tparam  ct   coordinate type
+   *  \tparam  dim  dimension of the space
+   */
   template< class ct, int dim >
-  class GlobalMesh
+  class SPGlobalMesh
   {
-    typedef GlobalMesh< ct, dim > This;
+    typedef SPGlobalMesh< ct, dim > This;
 
   public:
     typedef ct ctype;
@@ -24,23 +35,42 @@ namespace Dune
     typedef FieldVector< ctype, dimension > GlobalVector;
     typedef SPMultiIndex< dimension > MultiIndex;
 
-    explicit GlobalMesh ( const GlobalVector &h )
+    /** \brief constructor
+     *
+     *  \param[in]  h  mesh width
+     *
+     *  \note The origin will be zero in this case.
+     */
+    explicit SPGlobalMesh ( const GlobalVector &h )
     : origin_( 0 )
     {
       for( int i = 0; i < dimension; ++i )
         h_[ i ] = std::abs( h[ i ] );
     }
 
-    GlobalMesh ( const GlobalVector &h, const GlobalVector &origin )
+    /** \brief constructor
+     *
+     *  \param[in]  h  mesh width
+     *  \param[in]  x  any mesh point
+     *
+     *  \note The origin will in general not coincide with x.
+     */
+    SPGlobalMesh ( const GlobalVector &h, const GlobalVector &x )
     {
       for( int i = 0; i < dimension; ++i )
       {
         h_[ i ] = std::abs( h[ i ] );
-        origin_[ i ] = origin[ i ] - std::floor( origin[ i ] / h_[ i ] ) * h_[ i ];
+        origin_[ i ] = x[ i ] - std::floor( x[ i ] / h_[ i ] ) * h_[ i ];
       }
     }
 
-    GlobalMesh ( const GlobalVector &a, const GlobalVector &b, const MultiIndex &cells )
+    /** \brief construct a global mesh from a split rectangle
+     *
+     *  \param[in]  a      corner of the rectangle
+     *  \param[in]  b      corner of the rectangle opposite to a
+     *  \param[in]  cells  number of cells in each direction
+     */
+    SPGlobalMesh ( const GlobalVector &a, const GlobalVector &b, const MultiIndex &cells )
     {
       for( int i = 0; i < dimension; ++i )
       {
@@ -49,6 +79,12 @@ namespace Dune
       }
     }
 
+    /** \brief obtain the coordinates of a mesh point
+     *
+     *  \param[in]  id  id of the mesh point
+     *
+     *  \returns coordinates of the mesh point in space
+     */
     GlobalVector coordinate ( const MultiIndex &id ) const
     {
       GlobalVector coordinate( origin_ );
@@ -57,6 +93,12 @@ namespace Dune
       return coordinate;
     }
 
+    /** \brief obtain the id of the nearest mesh point
+     *
+     *  \param[in]  x  position of a point in space
+     *
+     *  \returns id of the nearest mesh point
+     */
     MultiIndex id ( const GlobalVector &x )
     {
       MultiIndex id;
@@ -65,14 +107,29 @@ namespace Dune
       return id;
     }
 
+    /** \brief determine whether this mesh equals another
+     *
+     *  \param[in]  other    global mesh to test for equality with this one
+     *  \param[in]  epsilon  tolerance when comparing floating point numbers
+     *
+     *  \returns true if both meshes are equal up to the tolerance epsilon
+     */
     bool equals ( const This &other, const ctype epsilon ) const
     {
       const ctype eps2 = epsilon*epsilon;
       return ((h_ - other.h_).two_norm2() <= eps2) && ((origin_ - other.origin_).two_norm2() <= eps2);
     }
 
+    /** \brief obtain the origin of the mesh
+     */
     const GlobalVector &origin () const { return origin_; }
 
+    /** \brief obtain a refined version of this mesh
+     *
+     *  \param[in]  refinement  type of refinement to apply
+     *
+     *  \returns a refined version of the global mesh
+     */
     template< SPRefinementStrategy strategy >
     This refine ( const SPRefinement< dimension, strategy > &refinement ) const
     {
