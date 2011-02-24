325e0bb65c06891a7df0bcc44df5a357af664e25
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2011-02-24 17:30:45 +0000

    added persistentcontainer for SPGrid.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@516 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid.hh
+++ dune/grid/spgrid.hh
@@ -3,5 +3,6 @@
 
 #include <dune/grid/spgrid/grid.hh>
 #include <dune/grid/spgrid/hierarchicsearch.hh>
+#include <dune/grid/spgrid/persistentcontainer.hh>
 
 #endif // #ifndef DUNE_SPGRID_HH
---
+++ dune/grid/spgrid/persistentcontainer.hh
@@ -0,0 +1,34 @@
+#ifndef DUNE_SPGRID_PERSISTENTCONTAINER_HH
+#define DUNE_SPGRID_PERSISTENTCONTAINER_HH
+
+#include <dune/grid/utility/persistentcontainer.hh>
+
+namespace Dune
+{
+  // PersistentContainer for SPGrid
+  // ------------------------------
+
+  template< class ct, int dim, SPRefinementStrategy strategy,
+            class Comm, class Data, class Allocator >
+  class PersistentContainer< SPGrid< ct, dim, strategy, Comm >, 
+                             Data, Allocator >
+  : public PersistentContainerVector< SPGrid< ct, dim, strategy, Comm >, 
+                                      typename SPGrid< ct, dim, strategy, Comm >::HierarchicIndexSet,
+                                      std::vector<Data,Allocator> >
+  {
+    typedef SPGrid< ct, dim, strategy, Comm > Grid;
+    typedef PersistentContainerVector< Grid, typename Grid::HierarchicIndexSet, std::vector<Data,Allocator> > BaseType;
+
+  public:
+    //! Constructor filling the container with values using the default constructor 
+    //! Depending on the implementation this could be achieved without allocating memory
+    PersistentContainer ( const Grid &grid, const int codim, const Allocator &allocator = Allocator() )
+    : BaseType( grid, codim, grid.hierarchicIndexSet(), 1.1, allocator )
+    {
+      std::cout << "SPGrid perst cont " << std::endl;
+    }
+  };
+
+} // end namespace Dune
+
+#endif // end DUNE_SPGRID_PERSISTENTCONTAINER_HH
