4c57281e58b97d4730308c2322d62d52a34c448a
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2011-08-02 12:33:17 +0000

    specialization of twist utility.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@540 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/spgrid.hh
+++ dune/grid/spgrid.hh
@@ -5,4 +5,20 @@
 #include <dune/grid/spgrid/hierarchicsearch.hh>
 #include <dune/grid/spgrid/persistentcontainer.hh>
 
+#if HAVE_DUNE_FEM 
+#include <dune/fem/quadrature/caching/twistutility.hh>
+
+namespace Dune { 
+
+  // Specialization for SPGrid
+  //
+  template< class ct, int dim, SPRefinementStrategy strategy , class Comm > 
+  struct TwistUtility< SPGrid< ct, dim, strategy, Comm > > 
+    : public TwistFreeTwistUtility< SPGrid< ct, dim, strategy, Comm > > 
+  {
+  };
+}
+
+#endif // #if HAVE_DUNE_FEM 
+
 #endif // #ifndef DUNE_SPGRID_HH
