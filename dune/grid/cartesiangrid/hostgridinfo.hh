347d4699d294192c03af9c8295e4525f2933d69d
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2011-11-17 10:15:26 +0000

    remove CartesianGrid from the release branch
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/branches/release-2011.10@582 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/cartesiangrid/hostgridinfo.hh
+++
@@ -1,7 +0,0 @@
-#ifndef DUNE_CARTESIANGRID_HOSTGRIDINFO_HH
-#define DUNE_CARTESIANGRID_HOSTGRIDINFO_HH
-
-#include <dune/grid/cartesiangrid/hostgridinfo/generic.hh>
-#include <dune/grid/cartesiangrid/hostgridinfo/alugrid.hh>
-
-#endif // #ifndef DUNE_CARTESIANGRID_HOSTGRIDINFO_HH
