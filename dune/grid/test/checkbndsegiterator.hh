9318166eea539c5659f12709473b7685b087bfd9
Christoph Gersbacher <gersbach@mathematik.uni-freiburg.de>
2012-05-18 19:08:27 +0000

    Check whether all boundary intersections are found.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-spgrid/trunk@695 f45f52a6-3457-4210-b8e0-e9d642ddcde3

--- dune/grid/test/checkbndsegiterator.hh
+++ dune/grid/test/checkbndsegiterator.hh
@@ -2,9 +2,13 @@
 #define DUNE_SPGRID_CHECKBNDSEGITERATOR_HH
 
 //- C++ includes
+#include <algorithm>
 #include <cassert>
+#include <iostream>
+#include <vector>
 
 //- dune-common includes
+#include <dune/common/array.hh>
 #include <dune/common/exceptions.hh>
 
 //- dune-grid includes
@@ -19,16 +23,59 @@ namespace Dune
   void checkBoundarySegmentIterator ( const Dune::GridView< VT > &gridView )
   {
 #if DUNE_GRID_EXPERIMENTAL_GRID_EXTENSIONS
+    // get grid view types
     typedef Dune::GridView< VT > GridView;
-    typedef typename GridView::Implementation::BoundarySegmentIterator BoundarySegmentIterator;
-    typedef typename BoundarySegmentIterator::Intersection Intersection;
+    typedef typename GridView::IntersectionIterator IntersectionIterator;
+    typedef typename IntersectionIterator::Intersection Intersection;
     typedef typename Intersection::EntityPointer EntityPointer;
     typedef typename EntityPointer::Entity Entity;
 
+    // boundary segment iterator to be tested
+    typedef typename GridView::Implementation::BoundarySegmentIterator BoundarySegmentIterator;
+
+    // get index set
+    typedef typename GridView::IndexSet IndexSet;
+    const IndexSet &indexSet = gridView.indexSet();
+
+    // storage for all intersections in grid view
+    static const int numFaces = 2*GridView::dimension;
+    typedef Dune::array< int, numFaces > Data;
+    std::vector< Data > boundaryFace( indexSet.size( 0 ) );
+    for( typename std::vector< Data >::iterator vit = boundaryFace.begin(); vit != boundaryFace.end(); ++vit )
+      vit->fill( 0 );
+
+    // visit all boundary intersections
+    int bndIntersections = 0;
+    typedef typename GridView::template Codim< 0 >::Iterator Iterator;
+    const Iterator end = gridView.template end< 0 >();
+    for( Iterator it = gridView.template begin< 0 >(); it != end; ++it )
+    {
+      // get entity
+      const Entity &entity = *it;
+      if( !entity.hasBoundaryIntersections() )
+        continue;
+
+      // visit boundary intersections
+      const typename IndexSet::IndexType index = indexSet.index( entity );
+      const IntersectionIterator iend = gridView.iend( entity );
+      for( IntersectionIterator iit = gridView.ibegin( entity ); iit != iend; ++iit )
+      {
+        const Intersection &intersection = *iit;
+        if( intersection.boundary() )
+        {
+          boundaryFace[ index ][ intersection.indexInInside() ] = 1;
+          bndIntersections++;
+        }
+      }
+    }
+    std::cerr << "Found " << bndIntersections << " boundary intersections" << std::endl;
+
+    // now iterate over all boundary segments
+    int bndSegments = 0;
     const BoundarySegmentIterator bend = gridView.impl().boundarySegmentEnd();
     for( BoundarySegmentIterator bit = gridView.impl().boundarySegmentBegin(); bit != bend; ++bit )
     {
-      // get boundary segment
+      // dereference boundary segment iterator
       const Intersection &boundarySegment = *bit;
 
       // assert boundary segment is on boundary
@@ -43,30 +90,36 @@ namespace Dune
       const Entity &inside = *insidePtr;
 
       // find boundary segment in inside intersections 
-      bool foundIntersection = false;
-      typedef typename GridView::IntersectionIterator IntersectionIterator;
       const IntersectionIterator iend = gridView.iend( inside );
       for( IntersectionIterator iit = gridView.ibegin( inside ); iit != iend; ++iit )
       {
         const Intersection &intersection = *iit;
 
-        // check for equality should be symmetrical
-        if( intersection.impl().equals( boundarySegment.impl() ) 
-              && boundarySegment.impl().equals( intersection.impl() ) )
-        {
-          // assert boundary segment is not found twice
-          if( foundIntersection )
-            DUNE_THROW( GridError, "Boundary segment found twice in inside entity" );
+        // equality check should be symmetrical
+        if( intersection.impl().equals( boundarySegment.impl() ) != boundarySegment.impl().equals( intersection.impl() ) )
+          DUNE_THROW( GridError, "Comparison of intersection and boundary segment failed" );
 
+        // equality check should be symmetrical
+        if( intersection.impl().equals( boundarySegment.impl() ) )
+        {
           // assert that index in inside conincides
           if( intersection.indexInInside() != indexInInside )
             DUNE_THROW( GridError, "Index in inside of boundary segment and intersection in inside entity do not conincide" );
-          else
-            foundIntersection = true;
+
+          // assert boundary segment is not found twice
+          if( (boundaryFace[ indexSet.index( inside ) ][ indexInInside ] -= 1) < 0 )
+            DUNE_THROW( GridError, "Boundary segment found twice in inside entity" );
         }
       }
-      if( !foundIntersection )
-        DUNE_THROW( GridError, "Boundary segment not found in inside entity" );
+    }
+    std::cerr << "Found " << bndSegments << " boundary segments" << std::endl;
+
+    // check we found all boundary intersections
+    for( typename std::vector< Data >::iterator vit = boundaryFace.begin(); vit != boundaryFace.end(); ++vit )
+    {
+      for( size_t face = 0; face < vit->size(); ++face )
+        if( (*vit)[ face ] != 0 )
+          DUNE_THROW( GridError, "Not all boundary intersections were found" );
     }
 #else // #if DUNE_GRID_EXPERIMENTAL_GRID_EXTENSIONS
     std::cerr << ">>> Skipping check for BoundarySegmentIterator"
