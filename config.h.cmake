368851a039f363f52162b516fb2353fbbcfb6266
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2015-01-07 16:28:30 +0100

    add config.h.cmake

---
+++ config.h.cmake
@@ -0,0 +1,15 @@
+/* begin dune-spgrid */
+
+/* Define to the version of dune-spgrid */
+#define DUNE_SPGRID_VERSION "${DUNE_SPGRID_VERSION}"
+
+/* Define to the major version of dune-spgrid */
+#define DUNE_SPGRID_VERSION_MAJOR ${DUNE_SPGRID_VERSION_MAJOR}
+
+/* Define to the minor version of dune-spgrid */
+#define DUNE_SPGRID_VERSION_MINOR ${DUNE_SPGRID_VERSION_MINOR}
+
+/* Define to the revision of dune-spgrid */
+#define DUNE_SPGRID_VERSION_REVISION ${DUNE_SPGRID_VERSION_REVISION}
+
+/* end dune-spgrid */
