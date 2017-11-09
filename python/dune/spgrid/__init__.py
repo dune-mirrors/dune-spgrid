1000938a8e8569bdabb84eecb8e3e2057d243485
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-11-09 09:18:23 +0100

    [python] adapt use of configuration check

--- python/dune/spgrid/__init__.py
+++ python/dune/spgrid/__init__.py
@@ -1,42 +1,45 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import dune.common.checkconfiguration as checkconfiguration
-
-registry = { "grid": dict() }
+from dune.common.checkconfiguration import assertHave, ConfigurationError
 
 try:
-    checkconfiguration.assertHave("HAVE_DUNE_SPGRID")
-
-    def spBisectionGrid(domain, dimgrid=None, ctype="double"):
-        from ..grid.grid_generator import module, getDimgrid
-
-        if dimgrid is None:
-            dimgrid = getDimgrid(domain)
-
-        typeName = "Dune::SPGrid< " + ctype + ", " + str(dimgrid) + ", Dune::SPBisectionRefinement >"
-        includes = ["dune/grid/spgrid.hh", "dune/grid/spgrid/dgfparser.hh"]
-        gridModule = module(includes, typeName)
-
-        return gridModule.LeafGrid(gridModule.reader(domain))
+    assertHave("HAVE_DUNE_SPGRID")
+except ConfigurationError:
+    raise ImportError("DUNE module dune-spgrid was not found.")
 
 
-    def spIsotropicGrid(domain, dimgrid=None, ctype="double"):
-        from ..grid.grid_generator import module, getDimgrid
+def spBisectionGrid(domain, dimgrid=None, ctype="double"):
+    from ..grid.grid_generator import module, getDimgrid
 
-        if dimgrid is None:
-            dimgrid = getDimgrid(domain)
+    if dimgrid is None:
+        dimgrid = getDimgrid(domain)
 
-        typeName = "Dune::SPGrid< " + ctype + ", " + str(dimgrid) + ", Dune::SPIsotropicRefinement >"
-        includes = ["dune/grid/spgrid.hh", "dune/grid/spgrid/dgfparser.hh"]
-        gridModule = module(includes, typeName)
+    typeName = "Dune::SPGrid< " + ctype + ", " + str(dimgrid) + ", Dune::SPBisectionRefinement >"
+    includes = ["dune/grid/spgrid.hh", "dune/grid/spgrid/dgfparser.hh"]
+    gridModule = module(includes, typeName)
 
-        return gridModule.LeafGrid(gridModule.reader(domain))
+    return gridModule.LeafGrid(gridModule.reader(domain))
 
-    registry["grid"]["SPBisection"] = spBisectionGrid
-    registry["grid"]["SPIsotropic"] = spIsotropicGrid
 
-except checkconfiguration.ConfigurationError:
-    pass
+def spIsotropicGrid(domain, dimgrid=None, ctype="double"):
+    from ..grid.grid_generator import module, getDimgrid
+
+    if dimgrid is None:
+        dimgrid = getDimgrid(domain)
+
+    typeName = "Dune::SPGrid< " + ctype + ", " + str(dimgrid) + ", Dune::SPIsotropicRefinement >"
+    includes = ["dune/grid/spgrid.hh", "dune/grid/spgrid/dgfparser.hh"]
+    gridModule = module(includes, typeName)
+
+    return gridModule.LeafGrid(gridModule.reader(domain))
+
+
+registry = {}
+registry["grid"] = {
+        "SPBisection": spBisectionGrid,
+        "SPIsotropic": spIsotropicGrid
+    }
+
 
 if __name__ == "__main__":
     import doctest
